6e093d752777fec6368061edca04fd9eb2aba2b8
Shefali Gajbhiye <shefaliggg@gmail.com>
2022-05-14 14:05:58 +0530

    #51

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const fetchuser = require('../middleware/fetchuser');
+const Notes = require('../models/Notes');
+
+//route 1 : fetch all notes GET
 
 router.post('/',(req,res)=>{
    
