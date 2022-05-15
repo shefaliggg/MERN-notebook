2d79f0b76b1a578e1bf74c483df4ffeccbfa0330
Shefali Gajbhiye <shefaliggg@gmail.com>
2022-05-15 15:44:51 +0530

    #54

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -80,8 +80,6 @@ router.put('/updatenote/:id', fetchuser, async (req, res) => {
 router.delete('/deletenote/:id', fetchuser, async (req, res) => {
 
     try {
-        const { title, description, tag } = req.body;
-       
         //find the note to be updated n delete it
         let note = await Note.findById(req.params.id);
         if(!note){
