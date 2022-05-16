2fd9c94c6e368f83cae9510b07dae024655a5fb0
Shefali Gajbhiye <shefaliggg@gmail.com>
2022-05-16 12:41:31 +0530

    #57

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export class Home extends Component {
+  render() {
+    return (
+      <div className="container">
+      Home</div>
+    )
+  }
+}
+
+export default Home
